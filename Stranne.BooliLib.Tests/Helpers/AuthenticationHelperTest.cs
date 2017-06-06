681c0d5e4d1d60320b52cf419c23140da465f05e
Olof Stranne <olof.stranne@gmail.com>
2017-06-06 15:56:24 +0200

    Add authentication helper

---
+++ Stranne.BooliLib.Tests/Helpers/AuthenticationHelperTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Stranne.BooliLib.Helpers;
+using Xunit;
+
+namespace Stranne.BooliLib.Tests.Helpers
+{
+    [Trait("Area", "Services")]
+    public class AuthenticationHelperTest
+    {
+        [Theory]
+        [InlineData("Abc", "123")]
+        public void GetAuthenticationQuery(string callerId, string key)
+        {
+            var actual = AuthenticationService.GetAuthenticationQuery(callerId, key);
+
+            Assert.NotEmpty(actual);
+            Assert.Contains(new KeyValuePair<string, string>("callerId", callerId), actual);
+            Assert.True(actual.ContainsKey("time"));
+            Assert.True(actual.ContainsKey("unique"));
+            Assert.True(actual.ContainsKey("hash"));
+        }
+
+        [Theory]
+        [InlineData("abc123", "6367c48dd193d56ea7b0baad25b19455e529f5ee")]
+        [InlineData("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat orci ac arcu vulputate aliquet. In hac habitasse platea dictumst. In enim quam, iaculis a gravida suscipit, volutpat ac ipsum. Curabitur euismod orci sit amet tortor suscipit, eu ultricies felis fermentum. Aenean tincidunt convallis risus ac tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque suscipit elit et dignissim semper. Aliquam molestie neque non dui bibendum aliquet. Nunc sed sapien eget velit elementum auctor. Vestibulum turpis quam, efficitur in tempus quis, faucibus id justo. Nam interdum sem a turpis tincidunt maximus. Ut quis nisl sed turpis vulputate sollicitudin. Nulla sit amet arcu iaculis, molestie eros sit amet, bibendum tellus. Cras ut justo turpis. Ut vitae iaculis arcu, sed tempus libero. Quisque et justo metus.", "dafc5ce692c975a0b48cc1c9cca25611690a7174")]
+        public void CreateHash(string text, string expected)
+        {
+            var actual = AuthenticationService.CreateHash(text);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(16)]
+        public void CreateUniqueLength(int length)
+        {
+            var actual = AuthenticationService.CreateUnique(length);
+            Assert.Equal(length, actual.Length);
+        }
+
+        [Theory]
+        [InlineData(16)]
+        public void CreateUniqueCharacters(int length)
+        {
+            var actual = AuthenticationService.CreateUnique(length);
+            var regex = new Regex(@"^[a-zA-Z0-9]*$");
+            Assert.True(regex.IsMatch(actual));
+        }
+    }
+}
---
+++ Stranne.BooliLib/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Stranne.BooliLib.Helpers
+{
+    internal static class AuthenticationHelper
+    {
+        public static IDictionary<string, string> GetAuthenticationQuery(string callerId, string key)
+        {
+            var time = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            var unique = CreateUnique(16);
+            var hash = CreateHash(callerId + time + key + unique);
+
+            var authenticationQuery = new Dictionary<string, string>
+            {
+                {
+                    "callerId",
+                    callerId
+                },
+                {
+                    "time",
+                    time
+                },
+                {
+                    "unique",
+                    unique
+                },
+                {
+                    "hash",
+                    hash
+                }
+            };
+
+            return authenticationQuery;
+        }
+
+        internal static string CreateHash(string text)
+        {
+            var bytes = Encoding.ASCII.GetBytes(text);
+
+            byte[] hashedBytes;
+            using (var sha1 = SHA1.Create())
+            {
+                hashedBytes = sha1.ComputeHash(bytes);
+            }
+
+            var hashedText = BitConverter.ToString(hashedBytes);
+            hashedText = hashedText.Replace("-", "").ToLower();
+            return hashedText;
+        }
+
+        internal static string CreateUnique(int length)
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+
+            var bytes = new byte[length];
+            using (var crypto = RandomNumberGenerator.Create())
+            {
+                crypto.GetBytes(bytes);
+            }
+
+            var unique = new StringBuilder(length);
+            foreach (var b in bytes)
+            {
+                unique.Append(chars[b % chars.Length]);
+            }
+
+            return unique.ToString();
+        }
+    }
+}
