036662d6f959cd8b1ccc6afee2eb5da79c1041b1
Olof Stranne <olof.stranne@gmail.com>
2017-06-06 13:12:15 +0200

    Add script to generate local coverage report

---
+++ GenerateCoverageReport.ps1
@@ -0,0 +1,33 @@
+$rootPath = (Get-Item -Path ".\" -Verbose).FullName
+
+dotnet restore
+
+Set-Location 'Stranne.BooliLib.Tests'
+
+$resultsFile = $rootPath + '\artifacts\Stranne.BooliLib_coverage.xml'
+If (Test-Path $resultsFile){
+	Remove-Item $resultsFile
+}
+
+$openCoverConsole = $ENV:USERPROFILE + '\.nuget\packages\OpenCover\4.6.690\tools\OpenCover.Console.exe'
+$target = '-target:C:\Program Files\dotnet\dotnet.exe'
+$targetArgs = '-targetargs: test -c Release'
+$filter = '-filter:+[Stranne.BooliLib]* -[Stranne.BooliLib.Tests]*'
+$output = '-output:' + $resultsFile
+
+& $openCoverConsole $target $targetArgs '-register:user' $filter '-hideskipped:Filter' '-returntargetcode' '-mergeoutput' $output '-oldStyle'
+
+Set-Location $rootPath
+
+$reportGenerator = $ENV:USERPROFILE + '\.nuget\packages\reportgenerator\2.5.6\tools\ReportGenerator.exe'
+$reportOutput = $rootPath + '\artifacts\CoverageReport\'
+$reports = '-reports:' + $resultsFile
+$targetdir = '-targetdir:' + $reportOutput
+
+If (Test-Path $reportOutput){
+	Remove-Item $reportOutput -recurse
+}
+
+& $reportGenerator $reports $targetdir
+
+Invoke-Item ($reportOutput + 'index.htm')
