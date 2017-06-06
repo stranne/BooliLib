18fa579ca23d918ae92a98e7e02000f8b15fd25c
Olof Stranne <olof.stranne@gmail.com>
2017-06-06 13:55:24 +0200

    Add short date json converter

---
+++ Stranne.BooliLib.Tests/Converters/ShortDateJsonConverterTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Stranne.BooliLib.Converters;
+using Xunit;
+
+namespace Stranne.BooliLib.Tests.Converters
+{
+    [Trait("Area", "Converters")]
+    public class ShortDateJsonConverterTest
+    {
+        public static IEnumerable<object[]> WriteJsonData => new[]
+        {
+            new object[] { new DateTime(2014, 1, 10), "20140110" }
+        };
+
+
+        [Theory]
+        [MemberData(nameof(WriteJsonData))]
+        public void WriuteJson(DateTime value, string expected)
+        {
+            var actual = JsonConvert.SerializeObject(value, new ShortDateJsonConverter()).Trim('"');
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadJson()
+        {
+            Assert.Throws<NotImplementedException>(() => new ShortDateJsonConverter().ReadJson(null, null, null, null));
+        }
+
+        [Fact]
+        public void CanReadJson()
+        {
+            Assert.False(new ShortDateJsonConverter().CanRead);
+        }
+    }
+}
